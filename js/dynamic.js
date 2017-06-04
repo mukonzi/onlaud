8dfec7131cf7713e7bd90813d6fd7fceae25d222
remotesrc <stewmkonzi@gmail.com>
2017-06-04 22:39:24 +0200

    Add files via upload
    JavaScript folder

---
+++ js/dynamic.js
@@ -0,0 +1,20 @@
+
+$("#regform").hide();
+$("#audform").hide();
+$("#joinform").hide();
+
+
+$("#regbtn").click(function(event){
+		$("#regform").show();
+		$(this).hide();
+		});
+		
+$("#audbtn").click(function(event){
+		$("#audform").show();
+		$(this).hide();
+		});
+$("#joinbtn").click(function(event){
+	
+		$("#joinform").show();
+		$(this).hide();
+		});
